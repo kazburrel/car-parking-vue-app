71e28762b7bac9e64e595b98f68f6cd7dd4f57c6
kazburrel <oegbosionu@gmail.com>
2023-12-18 15:05:00 +0100

    Auth Register

--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,36 +1,35 @@
 <template>
-    <form @submit.prevent="() => { }" novalidate>
-        <div class="flex flex-col mx-auto md:w-96 w-full">
-            <h1 class="text-2xl font-bold mb-4 text-center">Register</h1>
-            <div class="flex flex-col gap-2 mb-4">
-                <label for="name" class="required">Name</label>
-                <input id="name" name="name" type="text" class="form-input" autocomplete="name" required />
-            </div>
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-md-6 p-5">
+                <form @submit.prevent="() => { }" novalidate>
+                    <div class="row h-100 justify-content-center align-items-center">
+                        <div class="mb-3">
+                            <label class="form-label">Name</label>
+                            <input type="text" class="form-control" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                         <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
 
-            <div class="flex flex-col gap-2 mb-4">
-                <label for="email" class="required">Email</label>
-                <input id="email" name="email" type="email" class="form-input" autocomplete="email" required />
-            </div>
-
-            <div class="flex flex-col gap-2 mb-4">
-                <label for="password" class="required">Password</label>
-                <input id="password" name="password" type="password" class="form-input" autocomplete="new-password"
-                    required />
-            </div>
-
-            <div class="flex flex-col gap-2">
-                <label for="password_confirmation" class="required">
-                    Confirm password
-                </label>
-                <input id="password_confirmation" name="password_confirmation" type="password" class="form-input"
-                    autocomplete="new-password" required />
-            </div>
-
-            <div class="border-t h-[1px] my-6"></div>
-
-            <div class="flex flex-col gap-2">
-                <button type="submit" class="btn btn-primary">Register</button>
             </div>
         </div>
-    </form>
+    </div>
 </template>
