d09fbde2bdcdd0d586d6512cb15ba3c6579990b9
kazburrel <oegbosionu@gmail.com>
2023-12-18 14:15:52 +0100

    setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+ 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: import('../views/HomeView.vue')
+      component: import('../views/HomeView.vue'),
     },
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('@/views/Auth/RegisterView.vue')
-    }
-  ]
+  ],
 })
-
+ 
 export default router
