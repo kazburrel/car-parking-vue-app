104cfdec14ea5b412f0709b1ad141ac4b74964e4
kazburrel <oegbosionu@gmail.com>
2023-12-19 19:07:46 +0100

    Auth Register Axios and error validation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "@/bootstrap.js";
 import './assets/main.css'
 import ValidationError from "@/components/ValidationError.vue";
+import IconSpinner from "@/components/IconSpinner.vue";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -13,5 +14,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.component("ValidationError", ValidationError);
+app.component("IconSpinner", IconSpinner);
 
 app.mount('#app')
--- src/stores/register.js
+++ src/stores/register.js
@@ -1,42 +1,47 @@
-import { reactive } from "vue";
-import { defineStore } from "pinia";
+import { reactive, ref } from 'vue'
+import { defineStore } from 'pinia'
 
-export const useRegister = defineStore("register", () => {
-    const errors = reactive({});
-    const form = reactive({
-    name: "",
-    email: "",
-    password: "",
-    password_confirmation: "",
-  });
- 
+export const useRegister = defineStore('register', () => {
+  const errors = reactive({})
+  const loading = ref(false)
+  const form = reactive({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  })
+
   function resetForm() {
-    form.name = "";
-    form.email = "";
-    form.password = "";
-    form.password_confirmation = "";
-   
-    errors.value = {};
+    form.name = ''
+    form.email = ''
+    form.password = ''
+    form.password_confirmation = ''
+
+    errors.value = {}
   }
-  
+
   async function handleSubmit() {
-    errors.value = {};
-   
+    if (loading.value) return
+
+    loading.value = true
+    errors.value = {}
+
     return window.axios
-      .post("auth/register", form)
+      .post('auth/register', form)
       .then((response) => {
-        console.log(response.data);
+        console.log(response.data)
       })
       .catch((error) => {
         if (error.response.status === 422) {
-          errors.value = error.response.data.errors;
+          errors.value = error.response.data.errors
         }
       })
       .finally(() => {
-        form.password = "";
-        form.password_confirmation = "";
-      });
+        form.password = ''
+        form.password_confirmation = ''
+        loading.value = false
+      })
   }
- 
-  return { form, errors, resetForm, handleSubmit };
-});
+
+  return { form, errors, resetForm, handleSubmit }
+})
